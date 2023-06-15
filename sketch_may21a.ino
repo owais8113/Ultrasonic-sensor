859203922088184d67e987592fe3c06775ce3b49
owais8113 <127936539+owais8113@users.noreply.github.com>
2023-06-15 23:41:11 +0530

    Add files via upload

---
+++ sketch_may21a.ino
@@ -0,0 +1,31 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid="Dhruv";
+const char* password = "07012003";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Wifi connecting to ");
+  Serial.println( ssid );
+
+  WiFi.begin(ssid,password);
+
+  Serial.println();
+  Serial.print("Connecting");
+
+  while( WiFi.status() != WL_CONNECTED ){
+      delay(500);
+      Serial.print(".");        
+  }
+  Serial.println();
+
+  Serial.println("Wifi Connected Success!");
+  Serial.print("NodeMCU IP Address : ");
+  Serial.println(WiFi.localIP() );
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
